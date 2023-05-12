60f70f035f6e536999ffb46097b055b248119b6d
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-12 12:44:18 -0400

    convert to links

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 const Users = () => {
   return (
     <div>
-      <h2>User 1</h2>
+      <Link to="1">User 1</Link>
+      <Link to="2">User 2</Link>
+      <Link to="3">User 3</Link>
+      {/* <h2>User 1</h2>
       <h2>User 2</h2>
-      <h2>User 3</h2>
+      <h2>User 3</h2> */}
       <Outlet />
     </div>
   );
