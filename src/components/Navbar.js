5da2112c21e413116ace333a727fdf531b365bfd
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-12 16:19:25 -0400

    conditionally render the login route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 
 const Navbar = () => {
+  const { user } = useAuth();
   return (
     <nav className="primary-nav">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/products">Products</NavLink>
       <NavLink to="/profile">Profile</NavLink>
-      <NavLink to="/login">Login</NavLink>
+      {!user && <NavLink to="/login">Login</NavLink>}
     </nav>
   );
 };
