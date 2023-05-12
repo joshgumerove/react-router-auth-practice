1c9f87c63dcd6fbe10172e09ad1ee6e448f4ed03
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-12 12:40:35 -0400

    dynamic routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import { NoMatch } from "./components/NoMatch";
 import { Products } from "./components/Products";
 import { FeaturedProducts } from "./components/FeaturedProducts";
 import { NewProducts } from "./components/NewProducts";
+import { Users } from "./components/Users";
+import { UserDetails } from "./components/UserDetails";
+import { Admin } from "./components/Admin";
 
 function App() {
   return (
@@ -16,12 +19,16 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/order-summary" element={<OrderSummary />} />
+        <Route path="/users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
         <Route path="/products" element={<Products />}>
           <Route index element={<FeaturedProducts />} />
           <Route path="featured" element={<FeaturedProducts />} />
           <Route path="new" element={<NewProducts />} />
         </Route>
-        <Route path="/order-summary" element={<OrderSummary />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
