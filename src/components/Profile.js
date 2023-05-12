ca471242f907dbda347de051017c6914277e1175
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-05-12 16:26:08 -0400

    add logout functionality

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { useAuth } from "./auth";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  const { user } = useAuth();
-  return <div>Welcome {user}</div>;
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+  return (
+    <>
+      <div>Welcome {user}</div>
+      <button onClick={handleLogout}>Logout</button>
+    </>
+  );
 };
 
 export { Profile };
